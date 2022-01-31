11b3858f8c8696ea17d53cf55bb7fb2f4d3fc964
Sahil <sahilsarin390@gmail.com>
2022-01-31 21:06:47 +0530

    21st

---
+++ Arrays/21-SubarrayZeroSum.py
@@ -0,0 +1,29 @@
+# A python program to find if
+# there is a zero sum subarray
+
+
+def subArrayExists(arr, n):
+    # traverse through array
+    # and store prefix sums
+    n_sum = 0
+    s = set()
+
+    for i in range(n):
+        n_sum += arr[i]
+
+        # If prefix sum is 0 or
+        # it is already present
+        if n_sum == 0 or n_sum in s:
+            return True
+        s.add(n_sum)
+
+    return False
+
+
+# Driver code
+arr = [-3, 2, 3, 1, 6]
+n = len(arr)
+if subArrayExists(arr, n) == True:
+    print("Found a sunbarray with 0 sum")
+else:
+    print("No Such sub array exits!")
